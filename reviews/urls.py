ba33996791275550c2ca077cb02978b013c5dad1
tashapark <pdy2564@naver.com>
2024-11-21 23:43:54 +0900

    :sparkles: :white_check_mark: nav, modal 에 따른 html 추가

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from reviews.views import ReviewListView
 
+urlpatterns = [
+    path('', ReviewListView.as_view(), name='review'),  # 클래스형 뷰로 변경
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView
+
 
 # Create your views here.
+class ReviewListView(LoginRequiredMixin, ListView):
+    template_name = 'review/review_list.html'  # 사용할 템플릿
+    context_object_name = 'reviews'  # 템플릿에서 사용할 컨텍스트 이름
+
+    def get_queryset(self):
+        pass
