2f235927c8d3a94f238d7db374409c589e9f5e2b
leesomin <mmxs7171@gmail.com>
2024-11-23 01:15:55 +0900

    :card_file_box: 모든 모델 추가(users 모델 이동)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class Users(AbstractUser):
+    # 전체적으로 blank=False 추가(username 제외)
+    username = models.CharField(primary_key=True, max_length=150, blank=False)
+    #password = models.CharField(max_length=150,  blank=False) #장고가 알아서함
+    fullname = models.CharField(max_length=150, blank=False)
+    address = models.CharField(max_length=150 , blank=False)
+    birth = models.DateField(blank=False) # 유저가 계속 쓴다고 생각하면, 넣고 연령대 계산을 하는 것이 더 나아보임.
+    # default='기타' 추가(MySQL에서 못불러옴)
+    gender = models.CharField(max_length=10,default='기타',blank=False)
+    # blank=False로 주기 위함 / MySQL에는 EmailField 타입이 없어 django에서 추가
+    # django에서 자동으로 주니까 빼기
+    email = models.EmailField(blank=False)
+
+    # 선호 정보 json으로 받아야 추후 처리가 유용
+    my_genre = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
+    my_play_mood = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
+    my_play_keyword = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
+    my_actor = models.CharField(max_length=20, default="없음", blank=False)  # 기본값 "없음"
+
+    class Meta:
+        # managed = False #포함X, 커스텀 필드들이 db에 반영되지 않는 문제 방지
+        db_table = 'users'
+
+    def __str__(self):
+        return self.username
--- common/models.py
+++ common/models.py
@@ -2,29 +2,160 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from accounts.models import Users
 
-class Users(AbstractUser):
-    # 전체적으로 blank=False 추가(username 제외)
-    username = models.CharField(primary_key=True, max_length=150, blank=False)
-    #password = models.CharField(max_length=150,  blank=False) #장고가 알아서함
-    fullname = models.CharField(max_length=150, blank=False)
-    address = models.CharField(max_length=150 , blank=False)
-    birth = models.DateField(blank=False) # 유저가 계속 쓴다고 생각하면, 넣고 연령대 계산을 하는 것이 더 나아보임.
-    # default='기타' 추가(MySQL에서 못불러옴)
-    gender = models.CharField(max_length=10,default='기타',blank=False)
-    # blank=False로 주기 위함 / MySQL에는 EmailField 타입이 없어 django에서 추가
-    # django에서 자동으로 주니까 빼기
-    email = models.EmailField(blank=False)
 
-    # 선호 정보 json으로 받아야 추후 처리가 유용
-    my_genre = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
-    my_play_mood = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
-    my_play_keyword = models.JSONField(default=list, blank=False)  # 빈 리스트로 기본값 설정
-    my_actor = models.CharField(max_length=20, default="없음", blank=False)  # 기본값 "없음"
+### 외래키는 전부 on_delete=models.CASCADE로 설정
+
+
+# Play_list : 연극 목록 모델 (common 앱에 위치)
+class Play_list(models.Model):
+    play_id = models.CharField(primary_key=True, max_length=50, blank=False)
+    play_name = models.CharField(max_length=300, blank=False)
+    play_reg_date = models.DateTimeField() # API에서 연극 리스트 불러온 날짜(DAG 트리거 날짜)
+    genre = models.CharField(max_length=300, blank=False) # 우리가 구분해서 추가할 장르
 
     class Meta:
-        # managed = False #포함X, 커스텀 필드들이 db에 반영되지 않는 문제 방지
-        db_table = 'users'
+        # managed = False
+        db_table = 'Play_list'
+
+# Theater_location : 공연장 주소 모델 (common 앱에 위치)
+class Theater_location(models.Model):
+    mt10id = models.CharField(primary_key=True, max_length=50) # 공연시설ID
+    theater_nm = models.CharField(max_length=300, blank=False) # 공연장명
+    addr = models.CharField(max_length=300, blank=False) # 공연장 전체 주소
+    # play_id = models.ForeignKey(Play_list, on_delete=models.CASCADE, db_column='play_id', blank=False)
+
+    class Meta:
+        # managed = False
+        db_table = 'Theater_location'
+
+# play_detail : 연극 상세 모델 (common 앱에 위치)
+class Play_detail(models.Model):
+    play_id = models.ForeignKey(Play_list, max_length=50, on_delete=models.CASCADE, db_column='play_id', blank=False)
+    play_name = models.CharField(max_length=300, blank=False)
+    genre = models.CharField(max_length=300, blank=False) # 우리가 구분해서 추가할 장르
+    play_strdate = models.CharField(max_length=300, blank=False) # 시작일
+    play_enddate = models.CharField(max_length=300, blank=False) # 종료일
+    theater_nm = models.CharField(max_length=300, blank=False)  # 공연장명
+    play_forcast = models.CharField(max_length=300, blank=False) # 출연진
+    play_runtime = models.CharField(max_length=50, blank=False)  # 공연시간
+    play_age = models.CharField(max_length=50, blank=False) # 관람 연령
+    play_guidance = models.CharField(max_length=300, blank=False) # 가격 안내
+    play_poster = models.CharField(max_length=300, blank=False) # 포스터 이미지 URL
+    loc = models.CharField(max_length=300, blank=False) # 공연장 위치 중 지역만(ex. 서울특별시)
+    play_status = models.CharField(max_length=50, blank=False) # 공연 상태
+    styurls_1 = models.CharField(max_length=300, blank=False) # 상세이미지
+    styurls_2 = models.CharField(max_length=300, blank=False)
+    styurls_3 = models.CharField(max_length=300, blank=False)
+    styurls_4 = models.CharField(max_length=300, blank=False)
+    mt10id = models.ForeignKey(Theater_location, max_length=50, on_delete=models.CASCADE, db_column='mt10id', blank=False) # 공연시설ID
+    dtguidance = models.CharField(max_length=300, blank=False) # 공연 시간 안내(ex.요일별 시간)
+    relate_1 = models.CharField(max_length=300, blank=False) # 예매처이름
+    relateurl_1 = models.CharField(max_length=300, blank=False) # 예매처url
+    relate_2 = models.CharField(max_length=300, blank=False)
+    relateurl_2 = models.CharField(max_length=300, blank=False)
+    relate_3 = models.CharField(max_length=300, blank=False)
+    relateurl_3 = models.CharField(max_length=300, blank=False)
+    relate_4 = models.CharField(max_length=300, blank=False)
+    relateurl_4 = models.CharField(max_length=300, blank=False)
+    relate_5 = models.CharField(max_length=300, blank=False)
+    relateurl_5 = models.CharField(max_length=300, blank=False)
+    relate_6 = models.CharField(max_length=300, blank=False)
+    relateurl_6 = models.CharField(max_length=300, blank=False)
+    relate_7 = models.CharField(max_length=300, blank=False)
+    relateurl_7 = models.CharField(max_length=300, blank=False)
+    relate_8 = models.CharField(max_length=300, blank=False)
+    relateurl_8 = models.CharField(max_length=300, blank=False)
+
+    class Meta:
+        # managed = False
+        db_table = 'Play_detail'
+        # 복합키(play_id가 외래키여서 기본키로 지정하지 않는 걸 권장함)
+        constraints = [
+            models.UniqueConstraint(fields=['play_id', 'play_name'], name='unique_Play_detail')
+        ]
+
+
+
+
+
+# Location : 지역명 모델 (common 앱에 위치)
+class Location(models.Model):
+    loc = models.CharField(primary_key=True, max_length=50, blank=False) # 지역명(ex. 서울특별시)
+
+    class Meta:
+        # managed = False
+        db_table = 'Location'
+
+
+
+# Play_rank : 연극 순위 모델 (common 앱에 위치)
+# class Play_rank(models.Model):
+#     rank_id = models.AutoField(primary_key=True) # PK용 필드
+#     rank = models.IntegerField(blank=False) # 순위
+#     play_id = models.CharField(max_length=300, blank=False)
+#     play_name = models.CharField(max_length=300, blank=False)
+#     play_period = models.CharField(max_length=300, blank=False) # 공연기간
+#     theater_nm = models.CharField(max_length=300, blank=False)  # 공연장명
+#     rank_area = models.CharField(max_length=50, blank=False) # 지역 (ex. 서울)
+#     play_poster = models.CharField(max_length=300, blank=False)
+#     ststypes = models.CharField(max_length=50, blank=False) # day/week/month
+#     link_area = models.CharField(max_length=50, blank=False) # csv 저장용
+#     rank_reg_date = models.DateTimeField() # API에서 연극 순위 불러온 날짜(DAG 트리거 날짜)
+#
+#     class Meta:
+#         # managed = False
+#         db_table = 'Play_rank'
+
+
+
+# Review: 리뷰 모델 (common 앱에 위치)
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    play_id = models.ForeignKey(Play_detail, max_length=50, on_delete=models.CASCADE, db_column='play_id', blank=False)
+    play_name = models.CharField(max_length=300, blank=False) # 리뷰 저장시 play_id를 통해 받아옴
+    theater_nm = models.CharField(max_length=300, blank=False) # 리뷰 저장시 play_id를 통해 받아옴
+    loc = models.CharField(max_length=300, blank=False) # 리뷰 저장시 play_id를 통해 받아옴(ex. 서울특별시)
+    review_title = models.CharField(max_length=255, blank=False)
+    review_contents = models.CharField(max_length=1000, blank=False)
+    star = models.ForeignKey('Star', on_delete=models.CASCADE, db_column='star')
+
+    # 오류나면 blank=True로 변경 예정
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='username', blank=False)
+    review_reg_date = models.DateTimeField(auto_now_add=True, blank=True) # 리뷰 작성 날짜(자동)
+
+    class Meta:
+        # managed = False
+        db_table = 'Review'
+
+
+
+# Star: 별점 모델 (common 앱에 위치)
+class Star(models.Model):
+    star_total = models.DecimalField(primary_key=True, max_digits=2, decimal_places=1)
+
+    class Meta:
+        # managed = False
+        db_table = 'Star'
 
     def __str__(self):
-        return self.username
+        # 숫자 -> 문자열 변환
+        return str(self.star_total)
+
+
+
+# Favorites : 즐겨찾기 모델 (common 앱에 위치)
+class Favorites(models.Model):
+    play_id = models.CharField(primary_key=True, max_length=50, blank=False)
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='username', blank=False)
+    play_name = models.CharField(max_length=300, blank=False)
+
+    class Meta:
+        # managed = False
+        db_table = 'Favorites'
+        # 복합키(play_id나 username으로만 지정하면 중복되는 값이 있을 수 있어 기본키 불가능)
+        constraints = [
+            models.UniqueConstraint(fields=['play_id', 'username'], name='unique_favorite')
+        ]
