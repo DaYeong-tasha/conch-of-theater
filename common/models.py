a218fc49d914bb3230213fbbb9fe5a56b83f54cd
leesomin <mmxs7171@gmail.com>
2024-11-23 20:56:51 +0900

    :card_file_box: 일부 모델 속성 수정

--- common/models.py
+++ common/models.py
@@ -14,7 +14,7 @@ class Play_list(models.Model):
     play_id = models.CharField(primary_key=True, max_length=50, blank=False)
     play_name = models.CharField(max_length=300, blank=False)
     play_reg_date = models.DateTimeField() # API에서 연극 리스트 불러온 날짜(DAG 트리거 날짜)
-    genre = models.CharField(max_length=300, blank=False) # 우리가 구분해서 추가할 장르
+    genre = models.CharField(max_length=300, blank=False, null=True) # 우리가 구분해서 추가할 장르
 
     class Meta:
         # managed = False
@@ -35,39 +35,39 @@ class Theater_location(models.Model):
 class Play_detail(models.Model):
     play_id = models.ForeignKey(Play_list, max_length=50, on_delete=models.CASCADE, db_column='play_id', blank=False)
     play_name = models.CharField(max_length=300, blank=False)
-    genre = models.CharField(max_length=300, blank=False) # 우리가 구분해서 추가할 장르
+    genre = models.CharField(max_length=300, blank=False, null=True) # 우리가 구분해서 추가할 장르
     play_strdate = models.CharField(max_length=300, blank=False) # 시작일
     play_enddate = models.CharField(max_length=300, blank=False) # 종료일
     theater_nm = models.CharField(max_length=300, blank=False)  # 공연장명
-    play_forcast = models.CharField(max_length=300, blank=False) # 출연진
-    play_runtime = models.CharField(max_length=50, blank=False)  # 공연시간
-    play_age = models.CharField(max_length=50, blank=False) # 관람 연령
-    play_guidance = models.CharField(max_length=300, blank=False) # 가격 안내
-    play_poster = models.CharField(max_length=300, blank=False) # 포스터 이미지 URL
+    play_forcast = models.CharField(max_length=300, blank=False, null=True) # 출연진
+    play_runtime = models.CharField(max_length=50, blank=False, null=True)  # 공연시간
+    play_age = models.CharField(max_length=50, blank=False, null=True) # 관람 연령
+    play_guidance = models.CharField(max_length=300, blank=False, null=True) # 가격 안내
+    play_poster = models.CharField(max_length=300, blank=False, null=True) # 포스터 이미지 URL
     loc = models.CharField(max_length=300, blank=False) # 공연장 위치 중 지역만(ex. 서울특별시)
     play_status = models.CharField(max_length=50, blank=False) # 공연 상태
-    styurls_1 = models.CharField(max_length=300, blank=False) # 상세이미지
-    styurls_2 = models.CharField(max_length=300, blank=False)
-    styurls_3 = models.CharField(max_length=300, blank=False)
-    styurls_4 = models.CharField(max_length=300, blank=False)
+    styurls_1 = models.CharField(max_length=300, blank=False, null=True) # 상세이미지
+    styurls_2 = models.CharField(max_length=300, blank=False, null=True)
+    styurls_3 = models.CharField(max_length=300, blank=False, null=True)
+    styurls_4 = models.CharField(max_length=300, blank=False, null=True)
     mt10id = models.ForeignKey(Theater_location, max_length=50, on_delete=models.CASCADE, db_column='mt10id', blank=False) # 공연시설ID
-    dtguidance = models.CharField(max_length=300, blank=False) # 공연 시간 안내(ex.요일별 시간)
-    relate_1 = models.CharField(max_length=300, blank=False) # 예매처이름
-    relateurl_1 = models.CharField(max_length=300, blank=False) # 예매처url
-    relate_2 = models.CharField(max_length=300, blank=False)
-    relateurl_2 = models.CharField(max_length=300, blank=False)
-    relate_3 = models.CharField(max_length=300, blank=False)
-    relateurl_3 = models.CharField(max_length=300, blank=False)
-    relate_4 = models.CharField(max_length=300, blank=False)
-    relateurl_4 = models.CharField(max_length=300, blank=False)
-    relate_5 = models.CharField(max_length=300, blank=False)
-    relateurl_5 = models.CharField(max_length=300, blank=False)
-    relate_6 = models.CharField(max_length=300, blank=False)
-    relateurl_6 = models.CharField(max_length=300, blank=False)
-    relate_7 = models.CharField(max_length=300, blank=False)
-    relateurl_7 = models.CharField(max_length=300, blank=False)
-    relate_8 = models.CharField(max_length=300, blank=False)
-    relateurl_8 = models.CharField(max_length=300, blank=False)
+    dtguidance = models.CharField(max_length=300, blank=False, null=True) # 공연 시간 안내(ex.요일별 시간)
+    relate_1 = models.CharField(max_length=300, blank=False, null=True) # 예매처이름
+    relateurl_1 = models.CharField(max_length=300, blank=False, null=True) # 예매처url
+    relate_2 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_2 = models.CharField(max_length=300, blank=False, null=True)
+    relate_3 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_3 = models.CharField(max_length=300, blank=False, null=True)
+    relate_4 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_4 = models.CharField(max_length=300, blank=False, null=True)
+    relate_5 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_5 = models.CharField(max_length=300, blank=False, null=True)
+    relate_6 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_6 = models.CharField(max_length=300, blank=False, null=True)
+    relate_7 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_7 = models.CharField(max_length=300, blank=False, null=True)
+    relate_8 = models.CharField(max_length=300, blank=False, null=True)
+    relateurl_8 = models.CharField(max_length=300, blank=False, null=True)
 
     class Meta:
         # managed = False
