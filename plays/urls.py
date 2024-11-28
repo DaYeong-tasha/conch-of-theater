ea457609ce6d65f63eda92d9f650780f30800b75
leesomin <mmxs7171@gmail.com>
2024-11-28 16:19:07 +0900

    :poop::sparkles::lipstick: 연극 상세-아직 작업 중

--- plays/urls.py
+++ plays/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from plays import views
 
+urlpatterns = [
+        path('play/<int:pk>/', views.play_detail, name='play_detail'),
 ]
