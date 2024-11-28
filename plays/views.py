0161d0baa506f9313f30735e46c80d58a8e5fda8
jhw240715@gmail.com <jhw240715@gmail.com>
2024-11-29 00:43:22 +0900

    🔀 merge: gitignore 충돌 해결

--- plays/views.py
+++ plays/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404
-from common.models import Play_detail, Review
-
+from common.models import Play_detail, Review, Theater_location
+from django.conf import settings
 
 def play_detail(request, pk):
-    play_detail = get_object_or_404(Play_detail, pk=pk)
+    play_detail = get_object_or_404(Play_detail, play_id=pk)
     reviews = Review.objects.filter(play_id=pk)
-    return render(request, 'plays/play_detail.html',
-                  {'play_detail': play_detail, 'reviews': reviews})
+    theater = get_object_or_404(Theater_location, mt10id=play_detail.mt10id)
+    
+    context = {
+        'play_detail': play_detail,
+        'reviews': reviews,
+        'theater': theater,
+        'kakao_map_api_key': settings.KAKAO_MAP_API_KEY
+    }
+    return render(request, 'plays/play_detail.html', context)
