62c74457051b4039c2b6fc4a89b07af2b675e8fa
conch of theater <tasha.dy2564@gmail.com>
2024-11-30 19:58:43 +0900

    :memo: models.py

--- common/models.py
+++ common/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-# Create your models here.
-from django.conf import settings
-from accounts.models import Users
+from django.conf import settings 
 
 
 ### 외래키는 전부 on_delete=models.CASCADE로 설정
