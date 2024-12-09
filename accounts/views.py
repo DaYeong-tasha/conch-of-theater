98c230941b27d6199567b18c16a2841a6d0580a6
ttn0a <ttn0a1@naver.com>
2024-12-09 15:49:47 +0900

    [마이페이지] 즐겨찾기 페이지 생성

--- accounts/views.py
+++ accounts/views.py
@@ -246,4 +246,24 @@ def reviews_edit(request, review_id):
     return render(request, 'accounts/profile_reviews_edit.html', {'form': form})  # 수정 페이지 렌더링
 
 
+#즐겨찾기♡
+def mypage_favorites(request):
+    # 현재 로그인한 사용자의 즐겨찾기한 연극 목록을 가져옵니다.
+    # 현재 로그인한 사용자가 즐겨찾기한 연극 목록 가져오기
+    favorite_plays = Play_list.objects.filter(favorite_users=request.user)
+
+    context = {
+        'favorite_plays': favorite_plays
+    }
+    return render(request, 'accounts/profile_favorites.html', context)
+    return redirect('login')
+
+
+def remove_from_favorites(request, play_id):
+    if request.user.is_authenticated:
+        play = get_object_or_404(Play_list, pk=play_id)
+        # 즐겨찾기에서 제거하는 로직 추가
+        play.favorite_users.remove(request.user)
+        return redirect('accounts:profile_favorites')  # 즐겨찾기 페이지로 리디렉션
+    return redirect('login')
 
